23fb91c2631699392df4a0a518470d9d1e082abe
Gunalvijay <144464847+Gunalvijay@users.noreply.github.com>
2025-04-15 22:53:16 +0530

    Add files via upload

---
+++ Mini projects/JS guessing game/index.js
@@ -0,0 +1,30 @@
+const minNum = 1;
+const maxNum = 100;
+const answer = Math.floor(Math.random() * (maxNum - minNum + 1)) + 1;
+
+let attempts = 0;
+let guess;
+let running = true;
+
+while(running){
+    guess = window.prompt(`Guess a number between ${minNum} - ${maxNum}`);
+    guess = Number(guess);
+    if(isNaN(guess)){
+        window.alert("Please enter a valid number");
+    }
+    else if(guess > maxNum || guess < minNum){
+        window.alert("Please enter a valid number");
+    }
+    else{
+        attempts++;
+        if(guess < answer){
+            window.alert("Too Low Try again");
+        }
+        else if(guess > answer){
+            window.alert("Too High Try again");
+        }
+        else{
+            window.alert(`Correct answer was ${answer}. It took you ${attempts} attempts`);;
+        }
+    }
+}
